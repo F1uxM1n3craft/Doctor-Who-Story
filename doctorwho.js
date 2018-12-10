5901e427a2c97d194c18560eeadd3b619280511c
F1uxM1n3craft <42870961+F1uxM1n3craft@users.noreply.github.com>
2018-12-10 09:02:58 -0500

    Second Edit (WIP)

--- doctorwho.js
+++ doctorwho.js
@@ -7,7 +7,6 @@ var choices = [];
 var answer = null;
 // story function variables
 var name = null;
-var spin = 0;
 
 var start = function () {
     setup(); // Helper Function
@@ -18,8 +17,8 @@ window.onload = start;  // Kicks off program
 
 function checkAnswers(answer) {
     switch(answer) {
-        case "Think some more":
-            thinkSomeMore();
+        case "Enter Police Box":
+            inPoliceBox();
             break;
         case "go in":
             enterForest();
@@ -29,10 +28,10 @@ function checkAnswers(answer) {
 
 
  function discoverPoliceBox(){  // FIRST SCENE (Default)- WELCOMES PLAYER
-  var name = prompt("what is your name?");
+  var name = prompt("What is your name?(This will be important later):");
   story("You are walking home from school and you then hear a weird sound from the woods.\
 \nSuddenly a strange blue police box comes into view.\
-You are not sure how it got there but you go up to it for a closer inspection.  What do you do?");
+\nYou are not sure how it got there but you go up to it for a closer inspection.  What do you do?");
   
   choices = ["Enter Police Box", "Ignore it", "Examine it"];
   answer = setOptions(choices);
@@ -41,39 +40,20 @@ You are not sure how it got there but you go up to it for a closer inspection.  
 //SCENE BREAK
 
 function inPoliceBox() {  // SET A SCENE
-	story("You decide to go inside this stange box"); 
-	choices = ["Candy house", "Fruit vender", "Field", "Hang out here", "spin in a circle"];
+	story("You decide to go inside this stange box and inside...What’s this?...it's bigger on the inside?\
+\nJust then you hear someone say “Welcome to my TARDIS, that\'s Time And Relative Dimensions in Space for short.\ 
+"\nI\'m The Doctor.” your not sure who this is but you ask \"Doctor Who?\". The Doctor then says \" Exactly so+name+Exactly so. So, all of time and space, where do you wanna start?\""); 
+	choices = ["", "Fruit vender", "Field", "Hang out here", "spin in a circle"];
 	answer = setOptions(choices);    
  }
   
  
-/* function spinInCircle() { // CHANGE VALUE IF RETURN
- *     spin = spin + 1;
- *    if (spin == 3) {
- *         story("Why are you doing that?\
- *         \n It makes me a little nauseous just to watching you\n\n");
- *         forest();
- *     } else if (spin == 4) {
- *         story("Seriously, please stop\
- *         \nI can't keep watching this\n\n");
- *        forest();
- *    } else if (spin == 5) {
- *         story("Oh god I have to go\n\n");
- *         forest();
- *    } else if (spin > 5) {
- *         story("We are sorry but the narrator for this bit went home sick.\n\n");
- *         forest();
- *     } else {
- *        story("You let you arm fly freely at your side as you spin in a circle.\
- *         \nAround and around you go as the world blurs in front of you\
- *         \nyou get dizzy and fall to the ground\
- *         \nyou take a moment to gather your thoughts then you stand back up\n\n");
- *         forest();
- *     }
+function ignoreBox() { // CHANGE VALUE IF RETURN
+    
  * }
  */
  
-/* function stayQuite() { // Message Example
+/* function examineBox() { // Message Example
  * messages=["so your too good to talk to me huh",
  *         "big old person too proud to talk to a pixie",
  *         " you hear the pixie fly away in disgust"];
